fa75375462e37d8d0e20e7bcceda3ce296ad58ef
ZahraBaharlouei <zbaharlo80@gmail.com>
2025-12-14 20:04:00 +0330

    impelementation of homework2

---
+++ HomeWork2/Assignment2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public class BankAccount
+{
+   
+    private int balance;
+
+   
+    public BankAccount(int initialBalance)
+    {
+        balance = initialBalance;
+    }
+
+   
+    public void Deposit(double amount)
+    {
+        if (amount > 0)
+        {
+            balance += (int)amount;
+            Console.WriteLine($"{amount} Deposited");
+        }
+        else
+        {
+            Console.WriteLine("The deposit amount must be positive.");
+        }
+    }
+
+    
+    public void Withdraw(double amount)
+    {
+        if (amount <= balance)
+        {
+            balance -= (int)amount;
+            Console.WriteLine($"{amount} has been withdrawn from your account.");
+        }
+        else
+        {
+            Console.WriteLine("Insufficient funds");
+        }
+    }
+
+   
+    public void ShowBalance()
+    {
+        Console.WriteLine($"Current balance: {balance}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BankAccount account = new BankAccount(1000); // موجودی اولیه 1000
+
+        account.ShowBalance();       // نمایش موجودی
+        account.Deposit(500);        // واریز 500
+        account.Withdraw(200);       // برداشت 200
+        account.Withdraw(2000);      // تلاش برای برداشت بیشتر از موجودی
+        account.ShowBalance();       // نمایش موجودی نهایی
+    }
+}
---
+++ HomeWork2/Assignment3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class Statistics
+{
+    
+    public void AnalyzeNumbers(int num1, ref int num2, out int num3)
+    {
+       
+        num3 = num1 + num2;
+
+        num2 = num2 * num2;
+
+        Console.WriteLine($"{num1}" );
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.Write("Enter number1: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter number2: ");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter number3: ");
+        int c = int.Parse(Console.ReadLine());
+
+      
+        Console.WriteLine("---Befor The Change---");
+        Console.WriteLine($" num1: {a}");
+        Console.WriteLine($" num2: {b}");
+        Console.WriteLine($" num3: {c}");
+
+       
+        Statistics stats = new Statistics();
+
+        
+        stats.AnalyzeNumbers(a, ref b, out c);
+
+       
+        Console.WriteLine("---After The Change---");
+        Console.WriteLine($" num1: {a}");
+        Console.WriteLine($" num2: {b}");
+        Console.WriteLine($" num3: {c}");
+    }
+}
